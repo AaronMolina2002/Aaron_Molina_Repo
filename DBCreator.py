6c8894f1558e398c1806b35198c8125aa6acf5f7
AaronMolina2002 <aem0063@mix.wvu.edu>
2022-09-02 16:31:51 -0400

    In-class assignment 2

---
+++ DBCreator.py
@@ -0,0 +1,19 @@
+"""
+This class tests the connection to your DB by creating a test table
+"""
+
+# Import the connector class
+import DBTruncator
+
+# Create a new instance of the DB
+my_db = DBTruncator.MyDB()
+
+# SQL command to create a new table
+sqlCommand = 'CREATE TABLE IF NOT EXISTS Molina_Aaron_Table (MID  VARCHAR, MName  VARCHAR);'
+
+# Message to display upon table creation. Not integrated yet.
+sqlMessage = 'Table Test created successfully. Please access pgAdmin to verify table was created successfully'
+
+# Execute the SQL command.
+my_db.query(sqlCommand, '')
+
---
+++ pythonTester.py
@@ -0,0 +1,10 @@
+# This file is to see my name print out in code.
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print('Aaron Molina')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
