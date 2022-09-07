cecd929f5bb95d3704df7a8e58d43ae4e15effa6
AaronMolina2002 <aem0063@mix.wvu.edu>
2022-09-07 15:59:54 -0400

    In-class assignment 2

---
+++ IfStatements.py
@@ -0,0 +1,16 @@
+"""
+This is an example of If statements
+"""
+
+# declare first variation with initial value 12
+number1 = 12
+
+# declare second variable with initial value 6
+number2 = 6
+
+# compare the two
+if number1 > number2:
+    print("The value of the first number {0} ".format(number1),"is greater than the value of the second number {0}".format(number2))
+else:
+    print("Value of the second number is less than the value of the first number")
+
