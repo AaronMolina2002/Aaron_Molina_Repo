2fbafc2510e1f8861548f353bc0d3a99f101a5c9
AaronMolina2002 <aem0063@mix.wvu.edu>
2022-09-09 16:38:36 -0400

    In-class assignment 2

---
+++ FancyCalculator.py
@@ -0,0 +1,40 @@
+"""
+This is a fancy-ish calculator.
+This accepts one user input, the equation
+prints out the output
+"""
+# Display a message to user.
+print("Hello user, this is a fancy calculator.")
+equation = (input("Provide me with an equation"))
+
+# extract the first value of the equation and convert it into an integer. Display the value of the first number.
+val1 = int(equation.split(' ')[0])
+print(val1)
+
+# extract the operation sign from the equation. Display the operation.
+op = equation.split(' ')[1]
+print(op)
+
+# extract the second value of the equation and convert it into an integer. Display the value of the second number.
+val2 = int(equation.split(' ')[2])
+print(val2)
+
+# create an if statement for the addition operation to allow the full equation to do addition.
+if op == "+":
+    total = str(val1 + val2)# add a variable named total to add both values.
+    print("The total is: " + total)# display message saying the total is and add the total variable with the message.
+
+# create an if statement for the subtraction operation to allow the full equation to do subtraction.
+if op == "-":
+    total = str(val1 - val2)# add a variable named total to subtract both values.
+    print("The total is: " + total)# display message saying the total is and add the total variable with the message.
+
+# create an if statement for the division operation to allow the full equation to do division.
+if op == "/":
+    total = str(val1 / val2)# add a variable named total to divide both values.
+    print("The total is: " + total)# display message saying the total is and add the total variable with the message.
+
+# create an if statement for the multiplication operation to allow the full equation to do multiplication.
+if op == "*":
+    total = str(val1 * val2)# add a variable named total to multiple both values.
+    print("The total is: " + total)# display message saying the total is and add the total variable with the message.
