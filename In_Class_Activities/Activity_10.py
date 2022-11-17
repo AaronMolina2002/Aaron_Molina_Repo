987c200b96b4e7f1018618ab0668e1887aac607d
AaronMolina2002 <aem0063@mix.wvu.edu>
2022-11-16 22:46:28 -0500

    Activity 12

--- In_Class_Activities/Activity_10.py
+++ In_Class_Activities/Activity_10.py
@@ -36,7 +36,20 @@ while Page_counter < Pages:
     # set newStart to equal to the added Start_counter
     newStart = Start_counter
     # set the initial link to have the newStart variable to be added to both &start= and &memo=
-    initial_link = f"https://bugs.launchpad.net/ubuntu/+bugs?field.searchtext=&field.status%3Alist=EXPIRED&field.status%3Alist=CONFIRMED&field.status%3Alist=TRIAGED&field.status%3Alist=INPROGRESS&field.status%3Alist=FIXCOMMITTED&field.status%3Alist=FIXRELEASED&field.importance%3Alist=UNKNOWN&field.importance%3Alist=UNDECIDED&field.importance%3Alist=CRITICAL&field.importance%3Alist=HIGH&field.importance%3Alist=MEDIUM&field.importance%3Alist=LOW&field.importance%3Alist=WISHLIST&field.information_type%3Alist=PUBLIC&field.information_type%3Alist=PUBLICSECURITY&field.information_type%3Alist=PRIVATESECURITY&field.information_type%3Alist=USERDATA&assignee_option=any&field.assignee=&field.bug_reporter=&field.bug_commenter=&field.subscriber=&field.structural_subscriber=&field.component-empty-marker=1&field.tag=&field.tags_combinator=ANY&field.status_upstream-empty-marker=1&field.has_cve.used=&field.omit_dupes.used=&field.omit_dupes=on&field.affects_me.used=&field.has_no_package.used=&field.has_patch.used=&field.has_branches.used=&field.has_branches=on&field.has_no_branches.used=&field.has_no_branches=on&field.has_blueprints.used=&field.has_blueprints=on&field.has_no_blueprints.used=&field.has_no_blueprints=on&search=Search&orderby=-importance&memo={newStart}&start={newStart}"
+    initial_link = f"https://bugs.launchpad.net/ubuntu/+bugs?field.searchtext=&field.status%3Alist=EXPIRED&field" \
+                   f".status%3Alist=CONFIRMED&field.status%3Alist=TRIAGED&field.status%3Alist=INPROGRESS&field.status" \
+                   f"%3Alist=FIXCOMMITTED&field.status%3Alist=FIXRELEASED&field.importance%3Alist=UNKNOWN&field" \
+                   f".importance%3Alist=UNDECIDED&field.importance%3Alist=CRITICAL&field.importance%3Alist=HIGH&field" \
+                   f".importance%3Alist=MEDIUM&field.importance%3Alist=LOW&field.importance%3Alist=WISHLIST&field" \
+                   f".information_type%3Alist=PUBLIC&field.information_type%3Alist=PUBLICSECURITY&field" \
+                   f".information_type%3Alist=PRIVATESECURITY&field.information_type%3Alist=USERDATA&assignee_option" \
+                   f"=any&field.assignee=&field.bug_reporter=&field.bug_commenter=&field.subscriber=&field" \
+                   f".structural_subscriber=&field.component-empty-marker=1&field.tag=&field.tags_combinator=ANY" \
+                   f"&field.status_upstream-empty-marker=1&field.has_cve.used=&field.omit_dupes.used=&field" \
+                   f".omit_dupes=on&field.affects_me.used=&field.has_no_package.used=&field.has_patch.used=&field" \
+                   f".has_branches.used=&field.has_branches=on&field.has_no_branches.used=&field.has_no_branches=on" \
+                   f"&field.has_blueprints.used=&field.has_blueprints=on&field.has_no_blueprints.used=&field" \
+                   f".has_no_blueprints=on&search=Search&orderby=-importance&memo={newStart}&start={newStart} "
     # printed out what page the code is on and how many total pages there are
     print(f"This is page #{Page_counter} of {Pages}")
     # printed out the updated link
